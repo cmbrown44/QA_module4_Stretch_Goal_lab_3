5b3559f57b92edf8dc30b7563ec478676785b3d1
cmbrown44 <105240374+cmbrown44@users.noreply.github.com>
2023-06-16 10:28:05 +0100

    dad

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ DATE=`date +%Y.%m.%d.%H.%M`
 FILE=~/output
 
 cd ./Task1
-docker build -t charliebrown44/test_jenkins_build:$DATE . >> ~/output
+sudo docker build -t charliebrown44/test_jenkins_build:$DATE . >> ~/output
 sudo docker run -itd -p 3000:3000 --name helloworld $FILE
 
