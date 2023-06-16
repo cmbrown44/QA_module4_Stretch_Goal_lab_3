9ee0f94a6649af6d14ef43781d996d629b3c8c2f
cmbrown44 <105240374+cmbrown44@users.noreply.github.com>
2023-06-16 11:07:02 +0100

    update port

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ DATE=`date +%Y.%m.%d.%H.%M`
 
 cd ./Task1
 docker build -t charliebrown44/test_jenkins_build:$DATE .
-docker run -itd -p 5000:5000 --name test_jenkins_build charliebrown44/test_jenkins_build:$DATE
+docker run -itd -p 5000:5500 --name test_jenkins_build charliebrown44/test_jenkins_build:$DATE
 
