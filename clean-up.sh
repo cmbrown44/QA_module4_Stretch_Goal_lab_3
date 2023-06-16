87e586fa606e05fe6aade8b247f79ccfd32dc69d
cmbrown44 <105240374+cmbrown44@users.noreply.github.com>
2023-06-16 10:57:41 +0100

    update clean up

--- clean-up.sh
+++ clean-up.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-containers=$(docker ps -aq)
 
-if [[ -n "$containers" ]]; then
-echo $containers
-docker stop $(docker ps -aq);
-docker rm $(docker ps -aq) || true
-else
-echo "No containers to clean up"
-fi
+docker stop $(docker ps -aq)
+docker rm -f $(docker ps -aq)
+docker rm -f $(docker images)
+
+# containers=$(docker ps -aq)
+
+# if [[ -n "$containers" ]]; then
+# echo $containers
+# docker stop $(docker ps -aq);
+# docker rm $(docker ps -aq) || true
+# else
+# echo "No containers to clean up"
+# fi
