e2797b215d732e977245295da2497373857321d0
cmbrown44 <105240374+cmbrown44@users.noreply.github.com>
2023-06-16 10:32:53 +0100

    updating container name

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DATE=`date +%Y.%m.%d.%H.%M`
-FILE=~/output
+
 
 cd ./Task1
-docker build -t charliebrown44/test_jenkins_build:$DATE . >> ~/output
-docker run -itd -p 3000:3000 --name helloworld $FILE
+docker build -t charliebrown44/test_jenkins_build:$DATE .
+docker run -itd -p 3000:3000 --name test_jenkins_build charliebrown44/test_jenkins_build:$DATE
 
